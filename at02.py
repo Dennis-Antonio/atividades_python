fb7e2c0c3be1fd03a53c02b0200af882a4ed7987
Dennis-Antonio <dennisvianadde@gmail.com>
2024-08-30 19:15:32 -0300

    uplod

---
+++ at02.py
@@ -0,0 +1,11 @@
+def incrementar_contador():
+    global contador 
+    contador += 1
+    print(f"Contador atualizado: {contador}")
+    
+    mensagem = "Este é o valor atual do contador." 
+    print(mensagem)  
+
+incrementar_contador()
+
+print('mensagem')
---
+++ index.py
@@ -0,0 +1,8 @@
+nome = 'Dennis'
+idade = 19
+cidade = 'imperatriz'
+
+def informacoes_pessoais():
+    print(f"Meu nome é {nome}, tenho {idade} anos e moro em {cidade}.")
+
+informacoes_pessoais()
